ed272cfda2c3ecd447e5decf9b72ece09093179c
Joseph Evangelista <oootep123@gmail.com>
2023-05-15 21:34:57 -0700

    started implementing api side

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {CssBaseline, Grid} from '@material-ui/core';
 
+import {getPlacesData} from './api'
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
-// import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({lat: 0, lng: 0});
+    const [bounds, setBounds] = useState(null);
+    
+    useEffect(() => {
+        console.log(coordinates, bounds);
+
+        getPlacesData()
+            .then((data) => {
+                console.log(data);
+                
+                setPlaces(data);
+            })
+    }, []);
+
     return (
         <>
             <CssBaseline />
@@ -16,7 +33,11 @@ const App = () => {
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map />
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,14 +7,27 @@ import Rating from '@material-ui/lab';
 import useStyles from './styles';
 import { GoogleMap } from '@react-google-maps/api';
 
-const Map = () => {
+const Map = ({setCoordinates, setBounds, coordinates}) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
 
-    const coordinates = {lat: 0, lng: 0};
+    // const coordinates = {lat: 0, lng: 0};
     return (
         <div className={classes.mapContainer}>
-            <GoogleMapReact bootstrapURLKeys={{key: 'AIzaSyB49M_oJeJMDQvvOJ3jc3ZwTvazjSsh8BU'}} defaultCenter={coordinates} center={coordinates} defaultZoom={14} margin={[50, 50, 50, 50]} options={''} onChange={''} onChildClick={''}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{key: 'AIzaSyB49M_oJeJMDQvvOJ3jc3ZwTvazjSsh8BU'}} 
+                defaultCenter={coordinates} 
+                center={coordinates} 
+                defaultZoom={14} 
+                margin={[50, 50, 50, 50]} 
+                options={''} 
+                onChange={(e) => {
+                    console.log(e)
+                    setCoordinates({lat: e.center.lat, lng: e.center.lng});
+                    setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+                }} 
+                onChildClick={''}
+            >
 
             </GoogleMapReact>
         </div>
